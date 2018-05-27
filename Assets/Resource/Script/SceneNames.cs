ce8b3cbdcfa0ea6e72224ec931b939033ce9a502
Gailbert Huang <gailbert.h@anino.co>
2018-05-27 18:34:42 +0800

    The Beginning

---
+++ Assets/Resource/Script/SceneNames.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+public class SceneNames 
+{
+	public const string LOADING_SCREEN = "LoadingScene";
+	public const string GAME_SCENE = "GameScene";
+	public const string GAME_UI = "GameUI";
+	public const string TITLE_SCENE = "TitleScene";
+}
