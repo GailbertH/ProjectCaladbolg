b0c6821b1c13a20f94afe6a24d2cd38b8cd6342a
Gailbert Huang <gailbert.h@anino.co>
2019-02-17 19:15:16 +0800

    Something

---
+++ Assets/Resource/Script/UI/UIGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIGame : MonoBehaviour 
+{
+	[SerializeField] private Camera mainCamera;
+	[SerializeField] private UIGameJoyStick gameJoyStick;
+	[SerializeField] private Animation loadingScreen;
+
+	bool isDragging = false;
+	private static UIGame instance;
+	public static UIGame Instance { get { return instance; } }
+
+	void Awake()
+	{
+		instance = this;
+		mainCamera.gameObject.SetActive (false);
+	}
+		
+	public bool OnJoyStickDrag
+	{
+		set{ isDragging = value; }
+		get{ return isDragging; }
+	}
+}
